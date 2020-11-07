800ed1494eb9d5251eab4def8a568ca1b11605f3
Oscar Enrique Ureña Márquez <oscar.enrique.urena@gmail.com>
2020-11-07 11:30:24 -0600

    Changed folder structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyspectra", # Replace with your own username
+    version="0.0.1",
+    author="Oscar Ureña",
+    author_email="oscar.enrique.urena@gmail.com",
+    description="A  python package designed to work with spectroscopy data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OEUM/PySpectra",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','pandas','spc','scipy'],
+    keywords=['spectroscopy', 'nir', 'ftir', 'raman', 'spc','dx','foss','viavi','grams']
+)
